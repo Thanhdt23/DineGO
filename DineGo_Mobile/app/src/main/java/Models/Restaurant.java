fb9aaa0f6aa452e9a4eab312d2e082eb37134db1
stephen <minhthang32095@gmai.com>
2025-03-23 22:54:01 +0700

    add_session_homeSceen_searchByName_searchByType

--- DineGo_Mobile/app/src/main/java/Models/Restaurant.java
+++ DineGo_Mobile/app/src/main/java/Models/Restaurant.java
@@ -4,18 +4,20 @@ public class Restaurant {
     private String name;
     private String address;
     private String phone;
+    private String type;
     private String imageUrl;
 
-    public Restaurant(String name, String address, String phone, String imageUrl) {
+    public Restaurant(String name, String address, String type, String imageUrl) {
         this.name = name;
         this.address = address;
-        this.phone = phone;
+        this.type = type;
         this.imageUrl = imageUrl;
     }
 
     public String getName() { return name; }
     public String getAddress() { return address; }
     public String getPhone() { return phone; }
+    public String getType() { return type; }
     public String getImageUrl() { return imageUrl; }
 }
 
