6e84126cd78ea3d6c05aac33de65a4835f45683e
Nguyễn Văn Khởi <khoinv.it@gmail.com>
2025-03-24 13:07:27 +0700

    add-search

--- DineGO_Client/Controllers/HomeController.cs
+++ DineGO_Client/Controllers/HomeController.cs
@@ -28,17 +28,17 @@ namespace DineGO_Client.Controllers
             return View();
         }
 
-      public async Task<IActionResult> Search(string name, string address)
-{
-    if (string.IsNullOrEmpty(address))
-    {
-        address = "";
-    }
+        public async Task<IActionResult> Search(string name, string address)
+        {
+            if (string.IsNullOrEmpty(address))
+            {
+                address = "";
+            }
 
-    string searchUrl = string.Format(ApiEndpoints.RESTAURANT_SEARCH, name ?? "", address);
-    var restaurants = await _apiService.GetAsync<List<Restaurant>>(searchUrl);
-    return Json(restaurants);
-}
+            string searchUrl = string.Format(ApiEndpoints.RESTAURANT_SEARCH, name ?? "", address);
+            var restaurants = await _apiService.GetAsync<List<Restaurant>>(searchUrl);
+            return Json(restaurants);
+        }
 
 
         public IActionResult Privacy()
