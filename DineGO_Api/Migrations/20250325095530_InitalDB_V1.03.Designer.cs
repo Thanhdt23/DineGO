020e2dd80cbb9b684cd2374cbf60e0adb7d02122
stephen <minhthang32095@gmai.com>
2025-03-25 16:56:18 +0700

    update_db

--- DineGO_Api/Migrations/20250325095530_InitalDB_V1.03.Designer.cs
+++ DineGO_Api/Migrations/20250325095530_InitalDB_V1.03.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DineGO_Api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250325095530_InitalDB_V1.03")]
+    partial class InitalDB_V103
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
