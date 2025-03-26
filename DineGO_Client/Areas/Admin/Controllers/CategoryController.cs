9af5846aca3fb424b40ad728772c6e4b58da2867
Thanhdt23 <thanhduong785@gmail.com>
2025-03-26 18:49:09 +0700

    add_manage_admin

--- DineGO_Client/Areas/Admin/Controllers/CategoryController.cs
+++ DineGO_Client/Areas/Admin/Controllers/CategoryController.cs
@@ -1,21 +1,102 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Constant;
+using Core.Services;
+using DineGO_Client.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace DineGO_Client.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CategoryController : Controller
     {
-      public IActionResult Category()
+        private readonly ILogger<CategoryController> _logger;
+        private readonly ApiService _apiService;
+
+        public CategoryController(ILogger<CategoryController> logger, ApiService apiService)
         {
-            return View();
+            _logger = logger;
+            _apiService = apiService;
         }
+
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _apiService.GetAsync<List<Category>>(ApiEndpoints.CATEGORY);
+            return View(categories);
+        }
+        [HttpGet]
         public IActionResult AddCategory()
         {
             return View();
         }
-        public IActionResult UpdateCategory()
+
+        [HttpPost]
+        public async Task<IActionResult> AddCategory(Category category)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                var addData = new
+                {
+                    cate_id = category.cate_id,
+                    cate_type = category.cate_type,
+                    cate_description = category.cate_description
+                };
+                var response = await _apiService.PostAsync<object, dynamic>($"{ApiEndpoints.CATEGORY}", addData);
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateCategory(int id)
+        {    
+            var category = await _apiService.GetAsync<Category>($"{ApiEndpoints.CATEGORY}/id?ID={id}"); 
+            System.Console.WriteLine("cate"  + category);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> UpdateCategory(Category category)
+        {
+            System.Console.WriteLine(JsonConvert.SerializeObject(category));
+
+            if (category == null)
+            {
+                ModelState.AddModelError("", "Dữ liệu category không hợp lệ!");
+                return View(category);
+            }
+
+            var updateData = new
+            {
+                cate_id = category.cate_id,
+                cate_type = category.cate_type,
+                cate_description = category.cate_description
+
+            };
+            System.Console.WriteLine("updateData" + updateData);
+
+            // Gọi API để cập nhật
+            var response = await _apiService.PutAsync<object, dynamic>($"{ApiEndpoints.CATEGORY}", updateData);
+
+            if (response != null)
+            {
+                TempData["SuccessMessage"] = "Cập nhật category thành công!";
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Cập nhật category thất bại!";
+            }
+
+            return RedirectToAction("Index");
         }
         public IActionResult DeleteCategory()
         {
@@ -23,3 +104,4 @@ namespace DineGO_Client.Areas.Admin.Controllers
         }
     }
 }
+
