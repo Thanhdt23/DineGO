9b983d6343391710822a7180f6bb2a34c0b4bbef
Nguyễn Văn Khởi <khoinv.it@gmail.com>
2025-03-27 20:24:31 +0700

    add-change-password

--- DineGo_Mobile/app/src/main/java/Fragment/ProfileFragment.java
+++ DineGo_Mobile/app/src/main/java/Fragment/ProfileFragment.java
@@ -1,16 +1,28 @@
 package Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import androidx.fragment.app.Fragment;
-
+import com.example.dinego_mobile.ChangePasswordActivity;
 import com.example.dinego_mobile.R;
 
 public class ProfileFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        // Khai báo Button
+        Button changePasswordButton = view.findViewById(R.id.change_password_button);
+        changePasswordButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), ChangePasswordActivity.class);
+            startActivity(intent);
+        });
+
+        return view;
     }
 }
